40a9dfa018c396f97f99836c011cef856795d8af
Andrei Ignat <ignatandrei@yahoo.com>
2026-04-26 16:40:25 +0300

    refactgoring

---
+++ src/MSvg/MSvg.All/IconEntry.cs
@@ -0,0 +1,30 @@
+﻿using SvgIconGenerator;
+
+namespace MSvg.All;
+
+public sealed record IconEntry(
+    string Library,
+    IconDto Icon,
+    HashSet<string> SearchKeys,
+    string NormalizedName)
+{
+    public void AddSearchKeys(string key)
+    {
+        if(string.IsNullOrWhiteSpace(key)) return;
+        SearchKeys.Add(key);
+        SearchKeys.Add(key.Normalize());
+
+    }
+    public bool Contains(string name)
+    {
+        if(SearchKeys == null)   return false;
+        if (SearchKeys.Count == 0) return false;
+        return SearchKeys.Any(k => k.Contains(name, StringComparison.OrdinalIgnoreCase));
+    }
+    public int Distance(string name)
+    {
+        if (SearchKeys == null) return int.MaxValue;
+        if (SearchKeys.Count == 0) return int.MaxValue;
+        return SearchKeys.Min(k => Fastenshtein.Levenshtein.Distance(name, k));
+    }   
+}
---
+++ src/MSvg/MSvg.All/extens.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace MSvg.All;
+public static class MyExtensions
+{
+    extension(string value)
+    {
+        public string Normalize()
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return string.Empty;
+            }
+
+            var builder = new StringBuilder(value.Length);
+
+            foreach (var character in value)
+            {
+                if (char.IsLetterOrDigit(character))
+                {
+                    builder.Append(char.ToLowerInvariant(character));
+                }
+            }
+
+            return builder.ToString();
+
+        }
+    }
+
+
+}
