908fa4f3c33ba1a58121e365be2f47751a6a00e0
Andrei Ignat <ignatandrei@yahoo.com>
2026-05-27 21:46:50 +0300

    modify readme

---
+++ src/MSvg/Msvg.ModifyReadMe/Template/ReadmeTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Msvg.ModifyReadMe.Template;
+
+internal class ReadmeTemplate
+{
+    public KeyValuePair<string, int>[] LibrariesWithCount;
+    public int TotalIcons => LibrariesWithCount.Sum(kv => kv.Value);
+    public ReadmeTemplate()
+    {
+        LibrariesWithCount= [.. MSvg.All.IconLibraryDefinition.Libraries
+            .Select(l => new KeyValuePair<string, int>(l.Name, l.IconNames.Count))
+            .OrderBy(it=>it.Key)];
+
+    }
+
+}
