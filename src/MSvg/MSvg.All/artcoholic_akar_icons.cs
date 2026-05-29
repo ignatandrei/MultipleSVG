2916f52a26265f4894ecf51ed6781cd45489e9f3
Andrei Ignat <ignatandrei@yahoo.com>
2026-05-29 20:10:48 +0300

    artcoholic_akar_icons

---
+++ src/MSvg/MSvg.All/artcoholic_akar_icons.cs
@@ -0,0 +1,6 @@
+﻿namespace MSvg.All;
+
+
+[SvgIconGenerator.GenerateIcons("**/artcoholic_akar_icons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class artcoholic_akar_icons;
