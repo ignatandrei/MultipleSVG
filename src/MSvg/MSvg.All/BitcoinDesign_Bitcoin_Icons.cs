978b6324b41bb8b7f7285caa30f6a80de903d523
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-01 11:26:29 +0300

    BitcoinDesign_Bitcoin_Icons

---
+++ src/MSvg/MSvg.All/BitcoinDesign_Bitcoin_Icons.cs
@@ -0,0 +1,5 @@
+﻿namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/BitcoinDesign_Bitcoin_Icons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class BitcoinDesign_Bitcoin_Icons;
