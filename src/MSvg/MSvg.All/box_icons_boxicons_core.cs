d80bdec89a12bfede8caea80c49d4ef2d0fa09b9
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-01 17:09:49 +0300

    box_icons_boxicons_core

---
+++ src/MSvg/MSvg.All/box_icons_boxicons_core.cs
@@ -0,0 +1,6 @@
+﻿
+namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/box_icons_boxicons_core/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class box_icons_boxicons_core;
