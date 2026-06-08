abece85c9f1a14c770c2c0293f000e5dce83c9c2
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-08 17:15:07 +0300

    catppuccin_vscode_icons

---
+++ src/MSvg/MSvg.All/catppuccin_vscode_icons.cs
@@ -0,0 +1,6 @@
+﻿
+namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/catppuccin_vscode_icons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class catppuccin_vscode_icons;
