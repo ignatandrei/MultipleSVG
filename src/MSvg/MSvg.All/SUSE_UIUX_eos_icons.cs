24a1399581d9299edcbbf7c70cd14cb968747152
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-09 20:21:41 +0300

    SUSE_UIUX_eos_icons

---
+++ src/MSvg/MSvg.All/SUSE_UIUX_eos_icons.cs
@@ -0,0 +1,5 @@
+﻿namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/SUSE_UIUX_eos_icons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class SUSE_UIUX_eos_icons;
