8ad21f5dfe7301526797a16152984c786943d5e2
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-09 19:45:42 +0300

    fernandcf_duo_icons

---
+++ src/MSvg/MSvg.All/fernandcf_duo_icons.cs
@@ -0,0 +1,5 @@
+﻿namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/fernandcf_duo_icons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class fernandcf_duo_icons;
