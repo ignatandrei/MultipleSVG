9636c47e7e8ca5f9ad8e5ad5b114030fed51a190
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-10 16:17:10 +0300

    Update evil_icons_evil_icons.cs

--- src/MSvg/MSvg.All/evil_icons_evil_icons.cs
+++ src/MSvg/MSvg.All/evil_icons_evil_icons.cs
@@ -1,6 +1,6 @@
 ﻿namespace MSvg.All;
 
-[SvgIconGenerator.GenerateIcons("**/AndflettCascade/*.svg")]
+[SvgIconGenerator.GenerateIcons("**/evil_icons_evil_icons/*.svg")]
 [RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
 public static partial class evil_icons_evil_icons
 {
