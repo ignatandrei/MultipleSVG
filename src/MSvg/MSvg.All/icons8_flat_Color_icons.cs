3c9d58b61a2497fc767da0ef336e7d8dbf414265
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-10 21:25:08 +0300

    https://github.com/icons8/flat-Color-icons

---
+++ src/MSvg/MSvg.All/icons8_flat_Color_icons.cs
@@ -0,0 +1,5 @@
+namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/icons8_flat_Color_icons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class icons8_flat_Color_icons;
