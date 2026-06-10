cdf6a97cfe76bdf122764202d1b3f4f1ea975869
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-10 16:12:21 +0300

    evil icons icons

---
+++ src/MSvg/MSvg.All/evil_icons_evil_icons.cs
@@ -0,0 +1,7 @@
+﻿namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/AndflettCascade/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class evil_icons_evil_icons
+{
+}
