a06db7b34f5d828eb98c414296eafe4cb093b7ea
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-10 20:57:40 +0300

    lipis_flag_icons

---
+++ src/MSvg/MSvg.All/lipis_flag_icons_.cs
@@ -0,0 +1,5 @@
+namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/lipis_flag_icons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class lipis_flag_icons;
