4c3d3f0fc2ca58de966ed15b440d5d1393e9b98c
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-11 18:31:57 +0300

    health

---
+++ src/MSvg/MSvg.All/resolvetosavelives_healthicons.cs
@@ -0,0 +1,5 @@
+namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/resolvetosavelives_healthicons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class resolvetosavelives_healthicons;
