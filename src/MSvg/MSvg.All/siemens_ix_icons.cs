52e291d05fe5526ca63c9143c60517dce72501c7
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-12 07:06:37 +0300

    https://github.com/siemens/ix-icons

---
+++ src/MSvg/MSvg.All/siemens_ix_icons.cs
@@ -0,0 +1,5 @@
+namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/siemens_ix_icons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class siemens_ix_icons;
