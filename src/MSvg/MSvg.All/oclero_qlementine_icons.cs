f5bb9f257070c6e950a2137ab4e772a6ee543254
Andrei Ignat <ignatandrei@yahoo.com>
2026-06-13 08:31:21 +0300

    https://github.com/oclero/qlementine-icons/

---
+++ src/MSvg/MSvg.All/oclero_qlementine_icons.cs
@@ -0,0 +1,5 @@
+namespace MSvg.All;
+
+[SvgIconGenerator.GenerateIcons("**/oclero_qlementine_icons/*.svg")]
+[RSCG_TemplatingCommon.IGenerateDataFromAdditionalFiles("IconLookupShared")]
+public static partial class oclero_qlementine_icons;
